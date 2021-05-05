3876f68eeb9891538e4191afc89078783e91ed71
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-05-05 12:45:38 -0400

    rocket ships

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 🚀🚀🚀🚀🚀
         </a>
       </header>
     </div>
