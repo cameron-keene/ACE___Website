2b843695c845658b22addf9ee35782d95c54caa0
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-05-07 13:24:51 -0400

    install react-bootstrap on new cameronkeene branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Header from './Header'
+import BackgroundImage from './BackgroundImage'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <BackgroundImage />
+      
     </div>
   );
 }
---
+++ src/BackgroundImage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './BackgroundImage.css';
+
+function BackgroundImage(){
+	return(
+		<div className = 'BackgroundImage'>
+            <h1 className = "BackgroundImage_text">BackgroundImage</h1>
+            
+		</div>
+		
+
+	)
+}
+export default BackgroundImage
