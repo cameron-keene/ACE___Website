0cd31e51122d0e54b31ab79c1f423bf926fd3f8c
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-05-13 12:21:09 -0400

    Updates

---
+++ precache-manifest.43c52a13c6058abd966dbc8ffd81cffb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "637696093da8f8211327",
+    "url": "/Please_work/static/css/main.7a6c3b21.chunk.css"
+  },
+  {
+    "revision": "637696093da8f8211327",
+    "url": "/Please_work/static/js/main.3b6d7d07.chunk.js"
+  },
+  {
+    "revision": "e805bfab61e2dec9fe2d",
+    "url": "/Please_work/static/js/runtime~main.a7233536.js"
+  },
+  {
+    "revision": "bb076148a2d94ecb0139",
+    "url": "/Please_work/static/css/2.2eced045.chunk.css"
+  },
+  {
+    "revision": "bb076148a2d94ecb0139",
+    "url": "/Please_work/static/js/2.e8eeab95.chunk.js"
+  },
+  {
+    "revision": "3c3bd01b6d736621367c",
+    "url": "/Please_work/static/js/3.a46b8316.chunk.js"
+  },
+  {
+    "revision": "45f7f118007e6dd8a15c3704dfff8d5a",
+    "url": "/Please_work/static/media/logo-min.45f7f118.png"
+  },
+  {
+    "revision": "57cb6c0b9f4621667952789932c4d03b",
+    "url": "/Please_work/static/media/background.57cb6c0b.png"
+  },
+  {
+    "revision": "f2035137a4047f30a0baccb53ed8e5a9",
+    "url": "/Please_work/index.html"
+  }
+];
