e3c570aaec5bf6fb2df82cbb0870f0be53c20a1f
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-05-28 06:10:21 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Please_work/precache-manifest.a337bf21de61c444087b9c33784ddf20.js"
+  "/ACE_Website/precache-manifest.c514077679879845cf0a370668f4eec5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Please_work/index.html", {
+workbox.routing.registerNavigationRoute("/ACE_Website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
