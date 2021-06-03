ea5a7612183cdce8d92ce541326a5ef2cbe09f47
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-06-03 11:34:12 -0400

    Updates

---
+++ precache-manifest.1497d761c2aabcaf96ac64c8b699452d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "501823c671ad76d0a534",
+    "url": "/ACE_Website/static/css/main.e136019e.chunk.css"
+  },
+  {
+    "revision": "501823c671ad76d0a534",
+    "url": "/ACE_Website/static/js/main.bbd252e0.chunk.js"
+  },
+  {
+    "revision": "4e71242e61ab4b3b6a79",
+    "url": "/ACE_Website/static/js/runtime~main.53ce6f72.js"
+  },
+  {
+    "revision": "c3316ec158821419480a",
+    "url": "/ACE_Website/static/css/2.e4fc1281.chunk.css"
+  },
+  {
+    "revision": "c3316ec158821419480a",
+    "url": "/ACE_Website/static/js/2.90c19568.chunk.js"
+  },
+  {
+    "revision": "15ecee893436a9911e7e",
+    "url": "/ACE_Website/static/js/3.da43d1a3.chunk.js"
+  },
+  {
+    "revision": "4a18dcea29117770355b5c10cf0cde54",
+    "url": "/ACE_Website/static/media/kayton.4a18dcea.jpeg"
+  },
+  {
+    "revision": "45f7f118007e6dd8a15c3704dfff8d5a",
+    "url": "/ACE_Website/static/media/logo-min.45f7f118.png"
+  },
+  {
+    "revision": "57cb6c0b9f4621667952789932c4d03b",
+    "url": "/ACE_Website/static/media/background.57cb6c0b.png"
+  },
+  {
+    "revision": "ee9c101b1da80d5d44ca3fb1bb03d7e7",
+    "url": "/ACE_Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa111f9ff6845dc2f5310ca3b210a795.js"
+  "/ACE_Website/precache-manifest.1497d761c2aabcaf96ac64c8b699452d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ACE_Website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
