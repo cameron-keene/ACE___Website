34b172ad84d843ec425e4f0f9111a2756486eba9
cameron-keene <74378283+cameron-keene@users.noreply.github.com>
2021-06-03 11:33:32 -0400

    Updates

---
+++ precache-manifest.fa111f9ff6845dc2f5310ca3b210a795.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "501823c671ad76d0a534",
+    "url": "/static/css/main.e136019e.chunk.css"
+  },
+  {
+    "revision": "501823c671ad76d0a534",
+    "url": "/static/js/main.bbd252e0.chunk.js"
+  },
+  {
+    "revision": "4beb81da0d8464139c28",
+    "url": "/static/js/runtime~main.3f054430.js"
+  },
+  {
+    "revision": "c3316ec158821419480a",
+    "url": "/static/css/2.e4fc1281.chunk.css"
+  },
+  {
+    "revision": "c3316ec158821419480a",
+    "url": "/static/js/2.90c19568.chunk.js"
+  },
+  {
+    "revision": "15ecee893436a9911e7e",
+    "url": "/static/js/3.da43d1a3.chunk.js"
+  },
+  {
+    "revision": "4a18dcea29117770355b5c10cf0cde54",
+    "url": "/static/media/kayton.4a18dcea.jpeg"
+  },
+  {
+    "revision": "45f7f118007e6dd8a15c3704dfff8d5a",
+    "url": "/static/media/logo-min.45f7f118.png"
+  },
+  {
+    "revision": "57cb6c0b9f4621667952789932c4d03b",
+    "url": "/static/media/background.57cb6c0b.png"
+  },
+  {
+    "revision": "e20fd2a0fc7750b362471a1812c8a64f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ACE_Website/precache-manifest.4fb5f45fbe527493cb2cade861aa07e0.js"
+  "/precache-manifest.fa111f9ff6845dc2f5310ca3b210a795.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ACE_Website/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
