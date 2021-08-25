ba2f4ba115d92fd8e2b8f4d4fe587b61f28fc6ee
thomaspr1205 <60591404+thomaspr1205@users.noreply.github.com>
2021-08-25 11:26:09 -0400

    new build
    updated site

---
+++ build/precache-manifest.ce8e5da161a633387bec61c6f28de175.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7bc136488b104f8d50f",
+    "url": "/ACE_Website/static/css/main.7e427937.chunk.css"
+  },
+  {
+    "revision": "b7bc136488b104f8d50f",
+    "url": "/ACE_Website/static/js/main.a0d932a9.chunk.js"
+  },
+  {
+    "revision": "372533ac97130069595e",
+    "url": "/ACE_Website/static/js/runtime~main.0b6c52af.js"
+  },
+  {
+    "revision": "74c5ea097211964f4aaf",
+    "url": "/ACE_Website/static/css/2.ea044571.chunk.css"
+  },
+  {
+    "revision": "74c5ea097211964f4aaf",
+    "url": "/ACE_Website/static/js/2.427ae272.chunk.js"
+  },
+  {
+    "revision": "b4f836a90c0ba81971e1",
+    "url": "/ACE_Website/static/js/3.c585c3fa.chunk.js"
+  },
+  {
+    "revision": "45f7f118007e6dd8a15c3704dfff8d5a",
+    "url": "/ACE_Website/static/media/logo-min.45f7f118.png"
+  },
+  {
+    "revision": "57cb6c0b9f4621667952789932c4d03b",
+    "url": "/ACE_Website/static/media/background.57cb6c0b.png"
+  },
+  {
+    "revision": "084ce27eb9b176190e1cf7d7fd451648",
+    "url": "/ACE_Website/static/media/jpm-logo.084ce27e.jpg"
+  },
+  {
+    "revision": "fa9d5c02bd530d66cbf0544ea16b049e",
+    "url": "/ACE_Website/index.html"
+  }
+];
